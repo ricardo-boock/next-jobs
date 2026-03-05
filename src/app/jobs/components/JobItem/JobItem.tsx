3cc0c7931cfe7635cc71cd6a0ab72f1e75956dff
Ricardo Boock <r.boock.coding@proton.me>
2026-03-05 22:54:32 +0100

    fix: update responsive design for job results

--- src/app/jobs/components/JobItem/JobItem.tsx
+++ src/app/jobs/components/JobItem/JobItem.tsx
@@ -2,64 +2,74 @@ import { Heart, MapPin } from "lucide-react";
 import { LinkButton } from "@/components/LinkButton/LinkButton";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { cn, normalizeSalary } from "@/lib/utils";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { cleanJobTitle, cn, normalizeJobSalary } from "@/lib/utils";
 import { JobPreview } from "@/types/remotive";
 
 export const JobItem = ({ job }: { job: JobPreview }) => {
   return (
-    <article
-      key={job.id}
-      className={cn("grid gap-2 border border-gray-200 rounded-lg p-4")}
-    >
-      <div className={cn("flex justify-between")}>
-        <h1 className={cn("text-lg font-bold")}>{job.title}</h1>
+    <Card key={job.id} className={cn("gap-0 relative")}>
+      <CardHeader className={cn("flex justify-between mb-2 md:mb-0")}>
+        <CardTitle className={cn("text-lg max-w-[calc(100%-45px)]")}>
+          {cleanJobTitle(job.title)}
+        </CardTitle>
+
         <Button
           variant="link"
           size="icon"
           className={cn(
-            "rounded-lg text-muted-foreground hover:text-red-500 hover:bg-gray-100 transition-colors",
+            "rounded-lg text-muted-foreground hover:text-red-500 hover:bg-gray-100 transition-colors absolute right-0 -translate-x-1/2",
           )}
         >
-          <Heart className={cn("w-5 h-5")} />
+          <Heart className={cn("w-2 h-2")} />
         </Button>
-      </div>
+      </CardHeader>
 
-      <div className={cn("flex items-center gap-2 text-sm")}>
-        <span>{job.company_name}</span> <span>•</span>
-        <span className={cn("flex items-center gap-0.5")}>
-          <MapPin className={cn("w-3.25 h-3.25")} />
-          Remote ({job.candidate_required_location})
-        </span>
-      </div>
-      <div className={cn("flex gap-2 my-1")}>
-        {job.category && (
-          <Badge variant="secondary" className={cn("rounded-md")}>
-            {job.category}
-          </Badge>
-        )}
-
-        {job.job_type && (
-          <Badge variant="outline" className={cn("rounded-md capitalize")}>
-            {job.job_type.replace("_", "-")}
-          </Badge>
-        )}
-
-        {job.salary && (
-          <Badge variant="outline" className={cn("rounded-md")}>
-            {normalizeSalary(job.salary)}
-          </Badge>
-        )}
-      </div>
-      <div className={cn("flex items-end justify-between")}>
-        <LinkButton variant="outline" href={`/jobs/${job.id}`}>
-          View Details
-        </LinkButton>
-        <p className={cn("text-xs text-muted-foreground")}>
-          {new Intl.DateTimeFormat("en-US", {
-            dateStyle: "medium",
-          }).format(new Date(job.publication_date))}
-        </p>
-      </div>
-    </article>
+      <CardContent className={cn("grid gap-1")}>
+        <div
+          className={cn(
+            "flex flex-col md:flex-row items-start md:items-center gap-2 text-sm text-left",
+          )}
+        >
+          <span>{job.company_name}</span>{" "}
+          <span className={cn("hidden md:inline")}>•</span>
+          <span className={cn("flex items-center gap-0.5")}>
+            <MapPin className={cn("w-3.25 h-3.25")} />
+            Remote ({job.candidate_required_location})
+          </span>
+        </div>
+        <div className={cn("flex gap-2 flex-wrap my-1")}>
+          {job.category && (
+            <Badge variant="secondary" className={cn("rounded-md")}>
+              {job.category}
+            </Badge>
+          )}
+          {job.job_type && (
+            <Badge variant="outline" className={cn("rounded-md capitalize")}>
+              {job.job_type.replace("_", "-")}
+            </Badge>
+          )}
+          {job.salary && (
+            <Badge variant="outline" className={cn("rounded-md")}>
+              {normalizeJobSalary(job.salary)}
+            </Badge>
+          )}
+        </div>
+        <div className={cn("flex items-end justify-between mt-2")}>
+          <LinkButton
+            className={cn("no-underline")}
+            variant="outline"
+            href={`/jobs/${job.id}`}
+          >
+            View Details
+          </LinkButton>
+          <p className={cn("text-xs text-muted-foreground m-0")}>
+            {new Intl.DateTimeFormat("en-US", {
+              dateStyle: "medium",
+            }).format(new Date(job.publication_date))}
+          </p>
+        </div>
+      </CardContent>
+    </Card>
   );
 };
