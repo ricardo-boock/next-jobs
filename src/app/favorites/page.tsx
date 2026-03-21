16917d7ea2f911ae0fc501c644bd2006acf02ef2
Ricardo Boock <r.boock.coding@proton.me>
2026-03-21 14:38:52 +0100

    feat: add loading state

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,13 +1,14 @@
 import { Suspense } from "react";
 import { getJobs } from "@/lib/jobs";
 import { RemotiveResponse } from "@/types/remotive";
-import { FavoritesClient } from "./FavoritesClient/FavoritesClient";
+import { FavoritesClient } from "./components/FavoritesClient/FavoritesClient";
+import Loading from "./loading";
 
 export default async function Favorites() {
   const data: RemotiveResponse = await getJobs();
 
   return (
-    <Suspense fallback={<div>Loading favorites…</div>}>
+    <Suspense fallback={<Loading />}>
       <FavoritesClient jobs={data.jobs} />
     </Suspense>
   );
--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,13 +1,14 @@
+import { Suspense } from "react";
 import { getJobs } from "@/lib/jobs";
 import { RemotiveResponse } from "@/types/remotive";
 import JobsClient from "./components/JobsClient/JobsClient";
-import { Suspense } from "react";
+import Loading from "./loading";
 
 export default async function Jobs() {
   const data: RemotiveResponse = await getJobs();
 
   return (
-    <Suspense fallback={<div>Loading jobs…</div>}>
+    <Suspense fallback={<Loading />}>
       <JobsClient jobs={data.jobs} />
     </Suspense>
   );
